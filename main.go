bc22018bb1cb7f924cc2d4a9646addb531c495b9
Marc Bouvier <m.bouvier.dev@gmail.com>
2022-12-25 12:09:20 +0100

    wails application bootstrapping

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "baldir-dashboard-1",
+		Title:  "baldir-dashboard",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
